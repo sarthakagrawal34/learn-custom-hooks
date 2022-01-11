73ab33592d3d847bc06e00e8dd598780730a2038
Sarthak Agrawal <agrawal.sarthak2599@gmail.com>
2022-01-11 14:37:00 +0530

    Make project without using custom hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import React, {useState, useEffect} from "react";
 
-function App() {
+function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleEmailChange(e){
+      setEmail(e.target.value);
+  }
+  function handlePasswordChange(e){
+      setPassword(e.target.value);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+          <div>Email</div>
+          <div>
+              <input type="email" value ={email} onChange= {handleEmailChange} />
+          </div>
+          <br/>
+          <div>Password</div>
+          <div>
+              <input type="password" value ={password} onChange= {handlePasswordChange} />
+          </div>
+          <p>
+              <strong>
+                  <em> Email: </em>
+              </strong>
+              {email}
+              <strong>
+                  <em> Password: </em>
+              </strong>
+              {password}
+          </p>
+      </form>
+  );
+}
+
+function SignUpForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setconfirmPassword] = useState('');
+
+  function handleEmailChange(e){
+      setEmail(e.target.value);
+  }
+  function handlePasswordChange(e){
+      setPassword(e.target.value);
+  }
+  function handleconfirmPasswordChange(e){
+      setconfirmPassword(e.target.value);
+  }
+  
+  return (
+      <form>
+          <div>Email</div>
+          <div>
+              <input type="email" value ={email} onChange= {handleEmailChange} />
+          </div>
+          <br/>
+          <div>Password</div>
+          <div>
+              <input type="password" value ={password} onChange= {handlePasswordChange} />
+          </div>
+          <br/>
+          <div>Confirm Password</div>
+          <div>
+              <input type="password" value ={confirmPassword} onChange= {handleconfirmPasswordChange} />
+          </div>
+          <p>
+              <strong>
+                  <em> Email: </em>
+              </strong>
+              {email} |
+              <strong>
+                  <em> Password: </em>
+              </strong>
+              {password} |
+              <strong>
+                  <em> Confirm Password: </em>
+              </strong>
+              {confirmPassword}
+          </p>
+      </form>
+  );
+}
+function App(props) {
+  return (
+    <div className="App" style={{padding: 20}}>
+      <h2> Login </h2>
+      <LoginForm />
+      <h3> Sign Up</h3>
+      <SignUpForm/>
+      
     </div>
   );
 }
